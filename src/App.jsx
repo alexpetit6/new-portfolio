2627b9f1e8ec631eda31b9eefa7c0026609e9840
Alex Petit <alexpetit6@gmail.com>
2023-08-19 19:32:47 -0700

    getOpacity seems to get the correct opacity when an element is centered in the window

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import './App.css';
+import { useState } from 'react';
+import About from './components/About/About'
 
 function App() {
+  const [scrollY, setScrollY] = useState(0);
+
+  function getScrollY() {
+    setScrollY(window.scrollY)
+  }
+  window.addEventListener('scroll', getScrollY)
+
   return (
     <>
     <h1 className="display-1 text-center">Hello,</h1>
+    <About scrollY={scrollY}/>
     </>
   );
 }
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,3 +1,29 @@
-function About() {
+import './About.css'
+import { useState } from 'react'
+
+export default function About({ scrollY }) {
+  const [opacity, setOpacity] = useState(0.25)
+  // const [offset, setOffset] = useState(0)
   
+  function getOpacity(scrollY, setOpacity, id) {
+    const offsetTop = document.getElementById(id).offsetTop
+    const opacity = scrollY / (offsetTop / 2);
+    if (opacity <= 0.25) return
+    setOpacity(opacity);
+  }
+
+  // function getOffset() {
+  //   setOffset(document.getElementById('top').offsetTop)
+  // }
+
+  return (
+    <div className='text-center about-text' id='top'>
+      <h1>My name's Alex.</h1>
+      <button onClick={() => getOpacity(scrollY, setOpacity, 'top')}>get opacity</button>
+      {/* <button onClick={getOffset}>get offset</button> */}
+      <h3>opacity: {opacity}</h3>
+      {/* <h3>offset: {offset}</h3> */}
+      <h2>{scrollY}</h2>
+    </div>
+  )
 }
