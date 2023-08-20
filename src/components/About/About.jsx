a0a72f45de3705e433b46c16e8cdfd24aea8763d
Alex Petit <alexpetit6@gmail.com>
2023-08-19 21:20:24 -0700

    contact info added

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,5 @@
 import './About.css'
 import { useState } from 'react'
-
 export default function About({ scrollY }) {
   const [opacity, setOpacity] = useState(0.25)
   // const [offset, setOffset] = useState(0)
@@ -11,18 +10,28 @@ export default function About({ scrollY }) {
     opacity <= 0.25 ? setOpacity(0.25) : setOpacity(opacity);
   }
 
+  window.addEventListener('scroll', () => getOpacity(scrollY, setOpacity, 'top'))
   // function getOffset() {
   //   setOffset(document.getElementById('top').offsetTop)
   // }
 
   return (
-    <div className='text-center about-text' id='top'>
+    <div className='text-center about-text' id='top' style={ {opacity: `${opacity}`}}>
       <h1>My name's Alex.</h1>
-      <button onClick={() => getOpacity(scrollY, setOpacity, 'top')}>get opacity</button>
+      <h4>More specifically: Alexander Petit.</h4>
+      <div>
+        <h6 id='contact-header'>Here's some contact info: </h6> 
+        <a href="https://www.linkedin.com/in/alex-petit-large/" target='_blank' rel='noreferrer'><img className='contact-logos' src="images/logos/In-Blue-21.png" alt="LinkedIn"/></a>
+        <a href="https://github.com/alexpetit6" target='_blank' rel='noreferrer'><img className='contact-logos' src="images/logos/github-mark.png" alt="GitHub"/></a>
+        <h6>Renton, WA</h6>
+        &nbsp;|&nbsp;
+        <h6>alexpetit6@gmail.com</h6>
+      </div>
+      {/* <button onClick={() => getOpacity(scrollY, setOpacity, 'top')}>get opacity</button> */}
       {/* <button onClick={getOffset}>get offset</button> */}
-      <h3>opacity: {opacity}</h3>
+      {/* <h3>opacity: {opacity}</h3> */}
       {/* <h3>offset: {offset}</h3> */}
-      <h2>{scrollY}</h2>
+      {/* <h2>{scrollY}</h2> */}
     </div>
   )
 }
