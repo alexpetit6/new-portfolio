abc59064df25904d97343ecf981aa5a925a7eac4
Alex Petit <alexpetit6@gmail.com>
2023-08-21 17:42:04 -0700

    added collapse button

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,11 +9,16 @@ export default function About({ scrollY }) {
   const expandedRef = useRef(null);
   const expandAboutRef = useRef(null);
   
-  const expandedHeight = expandedRef.current ? expandedRef.current.scrollHeight : null;
+  
 
-  function handleExpandClick() {
-    expandedRef.current.style.height = `${expandedHeight}px`;
+  function handleExpand() {
+    expandedRef.current.style.height = `${expandedRef.current.scrollHeight}px`;
     expandAboutRef.current.style.height = '0'
+  }
+
+  function handleCollapse() {
+    expandedRef.current.style.height = '0'
+    expandAboutRef.current.style.height = `${expandAboutRef.current.scrollHeight}px`
   }
 
   window.addEventListener('scroll', () => getOpacity(scrollY, setOpacity, 'top'))
@@ -23,30 +28,33 @@ export default function About({ scrollY }) {
       <h1>My name's Alex.</h1>
       {/* <h4>More specifically: Alexander Petit.</h4> */}
       <div id='about-gif'>
-        <div ref={expandAboutRef} id='expand-about-btn' onClick={handleExpandClick}>
+        <div ref={expandAboutRef} id='expand-about-btn' onClick={handleExpand}>
           <h2>Want to know more about me?</h2>
         </div>
       </div>
       <div ref={expandedRef} id='about-expanded'>
         <h5>A lil’ tidbit</h5>
-        <p className='about-p'>
-          To be a little more specific my name is Alexander Petit. I’m a full-stack developer with a preference for front-end. 
-          I value the aesthetics of a website and the functionality equally. Both are important in their own regards, and I strive to balance them. 
-          But if there’s one thing you should really know about me it’s that I love to learn.
-        </p>
-        <p className='about-p'>
-          I keep myself in a constant state of learning and challenge. Take this portfolio for example; 
-          I could’ve easily finished a simple portfolio website in a day, maybe two, if I just wrote it 
-          in EJS, set up some simple routes and navigation, an easy navbar off of bootstrap, and a little 
-          CSS. Instead I went for a completely different approach from what I was already comfortable with 
-          to make a highly dynamic SPA. In the time I’ve spent with this project I have learned countless 
-          mechanics and features. I’ve been enlightened about handy Window properties such as scrollY and 
-          Element properties like offsetTop. With the two combined I made text that fades depending on a 
-          user’s scroll position. For me, the challenge of having to learn new methods and techniques to 
-          achieve a goal doesn’t stop me in any area of life, whether it’s rock climbing, Noita (a 
-          challenging video game with elements of gamified programming), or coding; the ominous presence 
-          of the unknown is for me an exciting invitation to learn and grow.
-        </p>
+        <button className='btn' onClick={handleCollapse}>X</button>
+        <div id='about-p'>
+          <p>
+            To be a little more specific my name is Alexander Petit. I’m a full-stack developer with a preference for front-end. 
+            I value the aesthetics of a website and the functionality equally. Both are important in their own regards, and I strive to balance them. 
+            But if there’s one thing you should really know about me it’s that I love to learn.
+          </p>
+          <p>
+            I keep myself in a constant state of learning and challenge. Take this portfolio for example; 
+            I could’ve easily finished a simple portfolio website in a day, maybe two, if I just wrote it 
+            in EJS, set up some simple routes and navigation, an easy navbar off of bootstrap, and a little 
+            CSS. Instead I went for a completely different approach from what I was already comfortable with 
+            to make a highly dynamic SPA. In the time I’ve spent with this project I have learned countless 
+            mechanics and features. I’ve been enlightened about handy Window properties such as scrollY and 
+            Element properties like offsetTop. With the two combined I made text that fades depending on a 
+            user’s scroll position. For me, the challenge of having to learn new methods and techniques to 
+            achieve a goal doesn’t stop me in any area of life, whether it’s rock climbing, Noita (a 
+            challenging video game with elements of gamified programming), or coding; the ominous presence 
+            of the unknown is for me an exciting invitation to learn and grow.
+          </p>
+        </div>
       </div>
       
 
