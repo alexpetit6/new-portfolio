39de9a2a147a5427295136b4e66e54e46089850c
Alex Petit <alexpetit6@gmail.com>
2023-08-23 11:10:21 -0700

    fixed position of hello

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import About from './components/About/About'
 import Projects from './components/Projects/Projects';
 
 function App() {
+  window.scrollTo(0,0)
   return (
     <>
+    <div id='anchor'>a</div>
     <h1 className="display-1 text-center">Hello,</h1>
     <About/>
     <Projects/>
