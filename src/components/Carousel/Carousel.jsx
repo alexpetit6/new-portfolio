f903cc2ecb2bc817602bbfda5a50d21faee23b56
Alex Petit <alexpetit6@gmail.com>
2024-03-12 13:18:50 -0700

    added animations for shadow and current and redid handleCycle

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -11,6 +11,7 @@ export default function Carousel() {
   });
 
   const refs = useRef([]);
+  const shadowRef = useRef(null);
 
   useEffect(() =>  {
     imgs.forEach((i, idx) => {
@@ -25,44 +26,63 @@ export default function Carousel() {
     return ((idx % arr.length) + arr.length) % arr.length;
   }
 
-  function handlePrev() {
+  function handleCycle(next) {
+    let operand = 1;
+    let type = 'forward';
+
+    if (!next) {
+      operand = -1;
+      type = 'back';
+    };
+
     setOrder({
-      prev: wrappedIndex(imgs, order.prev - 1),
-      current: wrappedIndex(imgs, order.current - 1),
-      next: wrappedIndex(imgs, order.next - 1)
+      prev: wrappedIndex(imgs, order.prev + operand),
+      current: wrappedIndex(imgs, order.current + operand),
+      next: wrappedIndex(imgs, order.next + operand)
     });
-    refs.current.forEach((r) => r.className=`${r.className.slice(0, 5)}  back`)
-    console.log(refs.current)
-  }
-  
-  function handleNext() {
-    setOrder({
-      prev: wrappedIndex(imgs, order.prev + 1),
-      current: wrappedIndex(imgs, order.current + 1),
-      next: wrappedIndex(imgs, order.next + 1)
-    });
-    refs.current.forEach((r) => r.className=`${r.className.slice(0, 5)}  forward`)
-    console.log(refs.current)
+
+    //trigger box-shadow animation
+    shadowRef.current.className = '';
+    //trigger reflow
+    void shadowRef.current.offsetWidth;
+    console.log(shadowRef.current.className, 'nada')
+    shadowRef.current.className = `box-shadow ${type}`;
+    refs.current.forEach((r) => r.className=`${r.className.slice(0, 5)}  ${type}`)
   }
 
+  // function handlePrev() {
+  //   setOrder({
+  //     prev: wrappedIndex(imgs, order.prev - 1),
+  //     current: wrappedIndex(imgs, order.current - 1),
+  //     next: wrappedIndex(imgs, order.next - 1)
+  //   });
+
+  //   refs.current.forEach((r) => r.className=`${r.className.slice(0, 5)}  back`)
+  //   console.log(refs.current)
+  // }
+  
+  // function handleNext() {
+  //   setOrder({
+  //     prev: wrappedIndex(imgs, order.prev + 1),
+  //     current: wrappedIndex(imgs, order.current + 1),
+  //     next: wrappedIndex(imgs, order.next + 1)
+  //   });
+  //   refs.current.forEach((r) => r.className=`${r.className.slice(0, 5)}  forward`)
+  //   console.log(refs.current)
+  // }
+
   //need seperate element to animate box-shadow with opacity for performance
-  const Imgs = imgs.map((i, idx) => {
-    return (
-      <div ref={el => refs.current[idx] = el}>
-        <div className='box-shadow'></div>
-        <img src={i} />
-      </div>
-    )
-  });
+  const Imgs = imgs.map((i, idx) => <img src={i} ref={el => refs.current[idx] = el}/>)
 
   return (
     // <Fancybox newClass="carousel-container">
       <div className="carousel">
-        <i onClick={handlePrev} className="fa-solid fa-chevron-left"></i>
+        <i onClick={() => handleCycle(false)} className="fa-solid fa-chevron-left"></i>
         <div className='carousel-imgs'>
+          <div ref={shadowRef} className='box-shadow'></div>
           {Imgs}
         </div>
-        <i onClick={handleNext} className="fa-solid fa-chevron-right"></i>
+        <i onClick={() => handleCycle(true)} className="fa-solid fa-chevron-right"></i>
       </div>
     // </Fancybox>
   )
