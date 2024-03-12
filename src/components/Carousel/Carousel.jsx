d33c3214699fa5f74164328e692c90a524f0e801
Alex Petit <alexpetit6@gmail.com>
2024-03-12 16:17:21 -0700

    fixed phasing issue

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -3,7 +3,7 @@ import { useState, createRef, useRef, useEffect } from 'react';
 import { Fancybox } from '@fancyapps/ui';
 
 export default function Carousel() {
-  const imgs = ['/images/climbing-thumbnail.jpg', '/images/katie-thumbnail.jpg', '/images/macro-thumbnail.jpg', '/images/svgc-thumbnail.jpg']
+  const imgs = ['/images/climbing-thumbnail.jpg', '/images/katie-thumbnail.jpg', '/images/svgc-thumbnail.jpg', '/images/macro-thumbnail.jpg']
   const [order, setOrder] = useState({
     prev: imgs.length - 1,
     current: 0,
@@ -45,31 +45,9 @@ export default function Carousel() {
     shadowRef.current.className = '';
     //trigger reflow
     void shadowRef.current.offsetWidth;
-    console.log(shadowRef.current.className, 'nada')
     shadowRef.current.className = `box-shadow ${type}`;
     refs.current.forEach((r) => r.className=`${r.className.slice(0, 5)}  ${type}`)
   }
-
-  // function handlePrev() {
-  //   setOrder({
-  //     prev: wrappedIndex(imgs, order.prev - 1),
-  //     current: wrappedIndex(imgs, order.current - 1),
-  //     next: wrappedIndex(imgs, order.next - 1)
-  //   });
-
-  //   refs.current.forEach((r) => r.className=`${r.className.slice(0, 5)}  back`)
-  //   console.log(refs.current)
-  // }
-  
-  // function handleNext() {
-  //   setOrder({
-  //     prev: wrappedIndex(imgs, order.prev + 1),
-  //     current: wrappedIndex(imgs, order.current + 1),
-  //     next: wrappedIndex(imgs, order.next + 1)
-  //   });
-  //   refs.current.forEach((r) => r.className=`${r.className.slice(0, 5)}  forward`)
-  //   console.log(refs.current)
-  // }
 
   //need seperate element to animate box-shadow with opacity for performance
   const Imgs = imgs.map((i, idx) => <img src={i} ref={el => refs.current[idx] = el}/>)
